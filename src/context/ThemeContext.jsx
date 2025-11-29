c6a675f75ebf271bf3da1e82086c203bad6720b9
Lind GECI <lindgeci1@gmail.com>
2025-11-29 20:03:23 +0100

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext({ theme: "dark", toggleTheme: () => {} });
 
-const STORAGE_KEY = "aaa-theme";
+const STORAGE_KEY = "dadaddadad-theme";
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState(() => {
